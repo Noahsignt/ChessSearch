7a0f95aa098d6d1bf540719d0d4705dcf9d986b4
noahsignt <noahvass01@gmail.com>
2023-04-01 19:34:51 +1100

    pawn movement

--- graphics.py
+++ graphics.py
@@ -1,10 +1,14 @@
 import pygame
+import chessboard
 
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+pygame.font.init() 
+my_font = pygame.font.SysFont('Arial Bold Italic', 30)
+chess = chessboard.Chessboard()
 
 while running:
     # poll for events
@@ -17,6 +21,8 @@ while running:
     screen.fill("white")
 
     # RENDER YOUR GAME HERE
+    text_surface = my_font.render(chess.print(), False, (0, 0, 0))
+    screen.blit(text_surface, (0,0))
 
     # flip() the display to put your work on screen
     pygame.display.flip()
